7284b1f0c9428ad04806477a9e67df043a2f563a
Agustin Schipani <agus-calderon-schip03@outlook.com>
2024-03-01 15:00:08 -0300

    Add project files.

---
+++ CSharpBasicTraning/EasyProblems/TwoSum.cs
@@ -0,0 +1,42 @@
+﻿namespace CSharpBasicTraning.EasyProblems
+{
+    internal class TwoSum
+    {
+        public static int[] MakeTheSumQuadratic(int[] nums, int target)
+        {
+            int arrLenght = nums.Length;
+
+            for (int i = 0; i < arrLenght - 1; i++) //A minus one is added so that the loops stops before the last element
+            {
+                for (int j = i + 1; j < arrLenght; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return [nums[i], nums[j]]; //A solution was found
+                    }
+                }
+            }
+
+            return []; //No solution was found
+        }
+
+        public static int[] MakeTheSumLinear(int[] nums, int target)
+        {
+            Dictionary<int, int> storage = [];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int value = nums[i];
+                int complement = target - value;
+
+                if (storage.TryGetValue(complement, out int v))
+                {
+                    return [i, v];
+                }
+
+                storage.TryAdd(nums[i], i);
+            }
+
+            return [];
+        }
+    }
+}
---
+++ CSharpBasicTraning/Program.cs
@@ -0,0 +1,14 @@
+﻿using CSharpBasicTraning.EasyProblems;
+
+namespace CSharpBasicTraning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] result = TwoSum.MakeTheSumLinear([5, 5, 12, 43, 29, 35], 55);
+
+            Console.WriteLine(result.Length != 0 ? $"The result is: [{result[0]}, {result[1]}]." : "The target was not reached.");
+        }
+    }
+}
